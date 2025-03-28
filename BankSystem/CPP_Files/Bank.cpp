bf61631b36a68e3128f1370edd5f384fcd87cf51
MGHD9 <163735227+MGHD9@users.noreply.github.com>
2025-03-28 12:12:23 +0100

    Bank System connection added

--- BankSystem/CPP_Files/Bank.cpp
+++ BankSystem/CPP_Files/Bank.cpp
@@ -5,14 +5,14 @@
 int main() {
     std::unique_ptr<Bank_Account> bankaccount;
     char letter;
-    std::cout << "Welcome to Bank System !" << std::endl;
+    std::cout << "Welcome to Bank System!" << std::endl;
     std::cout << "Create an account: yes (y) or no (n)? " << std::endl;
     std::cin >> letter;
 
     switch (letter) {
     case 'y':
         short pass;
-        std::cout << "You chose to create an account! Choose a password 4 numbers:" << std::endl;
+        std::cout << "You chose to create an account! Choose a password of 4 numbers:" << std::endl;
         std::cin >> pass;
         bankaccount = std::make_unique<Bank_Account>(0, pass);
         break;
@@ -24,8 +24,22 @@ int main() {
         break;
     }
 
-    std::cout << "Login to your account, enter your password : " << std::endl;
+    if (!bankaccount) {
+        std::cout << "No account found. Exiting..." << std::endl;
+        return 0;
+    }
 
+    short passlogin;
+    do {
+        std::cout << "Login to your account, enter your password: " << std::endl;
+        std::cin >> passlogin;
+
+        if (passlogin != bankaccount->GetBankPassword()) {
+            std::cout << "Incorrect password! Try again." << std::endl;
+        }
+    } while (passlogin != bankaccount->GetBankPassword());
+
+    std::cout << "\033[32mWelcome to your Bank Account!\033[0m" << std::endl;
 
     return 0;
 }
